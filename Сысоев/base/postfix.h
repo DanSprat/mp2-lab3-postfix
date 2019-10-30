635a0abfcf57bb051bee1aefb76ef877223ff333
Daniel Popov <popovdaniil2000@gmail.com>
2019-10-30 06:51:23 +0300

    aaaa

--- Сысоев/base/postfix.h
+++ Сысоев/base/postfix.h
@@ -3,18 +3,26 @@
 
 #include <string>
 #include "stack.h"
-
+#include <map>
 using namespace std;
 
 class TPostfix
 {
-  string infix;
-  string postfix;
+	map <string, int> priority;
+	map <string, int> operations;
+	string infix;
+	string postfix;
 public:
   TPostfix()
   {
-    infix = "a + b";
+	  operations = { { "+",1 },{ "-",2 },{ "*",3 },{ "/",4 },{ "sin",5 },{ "cos",6 } };
+	  priority = { { "+",4 },{ "-",4 },{ "*",3 },{ "/",3 },{ "(",1 },{ ")",-1 },{ "sin",2 },{ "cos",2 } };
   }
+  string Infix2Space(); // Преобразование строки в строку с проблеами после каждого операнда,переменной,константы
+  void SetInfix(string &a) { infix = a; }
+  bool isNumber(const string &a);// Проверка на константу
+  bool isAlt(const string &a); // Проверка на переменную
+  void isCorrectInfix();//Контроль правильности введенной строки
   string GetInfix() { return infix; }
   string GetPostfix() { return postfix; }
   string ToPostfix();
